3eab22d452d788f66b0537c5a345ce4918580d8a
stoeffel <schtoeffel@gmail.com>
2015-02-05 15:20:49 +0100

    add some dotfiles
    - add editorconfig
    - add jshintrc

--- index.js
+++ index.js
@@ -3,52 +3,52 @@
 require('harmony-reflect');
 require('es6-shim');
 
-function oMap(obj){
-	obj = obj || {};
+function oMap(obj) {
+    obj = obj || {};
 
-	const map = new Map();
+    const map = new Map();
 
-	Object.keys(obj).forEach(function(key) {
+    Object.keys(obj).forEach(function(key) {
         map.set(key, obj[key]);
-	});
+    });
 
-	const handlers = {
-		get: function(target,name){
-			if(name in target){
-				return target[name];
-			}
-			return target.get(name);
-		},
-		set: function (target,name,value){
-			return target.set(name,value);
-		},
-		has: function(target,value){
-			return target.has(value);
-		},
-		hasOwn: function(target,value){
-			return target.has(value);
-		},
-		deleteProperty: function(target,value){
-			return target.delete(value);
-		},
-		keys: function(target){
-			const k = target.keys();
-			const out = [];
+    const handlers = {
+        get: function(target, name) {
+            if (name in target) {
+                return target[name];
+            }
+            return target.get(name);
+        },
+        set: function(target, name, value) {
+            return target.set(name, value);
+        },
+        has: function(target, value) {
+            return target.has(value);
+        },
+        hasOwn: function(target, value) {
+            return target.has(value);
+        },
+        deleteProperty: function(target, value) {
+            return target.delete(value);
+        },
+        keys: function(target) {
+            const k = target.keys();
+            const out = [];
 
-			try{
-				while(true){
-					out.push(k.next().toString());
-				}
-			}finally{
-				return out;
-			}
-		},
-		enumerate: function(target){
-			return target.iterator();
-		}
-	};
+            try {
+                while (true) {
+                    out.push(k.next().toString());
+                }
+            } finally {
+                return out;
+            }
+        },
+        enumerate: function(target) {
+            return target.iterator();
+        }
+    };
 
-	return new Proxy(map,handlers);
+    return new Proxy(map, handlers);
 }
 
 
--- test.js
+++ test.js
@@ -1,37 +1,39 @@
 var oMap = require('./');
 var assert = require('assert');
 
-describe('creation',function(){
-	var map, a;
+describe('creation', function() {
+    var map, a;
 
     beforeEach(function() {
-		map = oMap({abc:123});
-        a = [1,2,3];
+        map = oMap({
+            abc: 123
+        });
+        a = [1, 2, 3];
     });
 
-	it('should be created',function(){
-		assert.equal(map.abc,123);
-	});
+    it('should be created', function() {
+        assert.equal(map.abc, 123);
+    });
 
-	it('should be created with a value 1',function(){
-		assert.equal(map.abc,123);
-	});
+    it('should be created with a value 1', function() {
+        assert.equal(map.abc, 123);
+    });
 
-	it('should be created with a value 2',function(){
-		assert.equal(map.get('abc'),123);
-	});
+    it('should be created with a value 2', function() {
+        assert.equal(map.get('abc'), 123);
+    });
 
-	it('should allow adding complex thigns',function(){
-		map[a] = 'xyz';
-	});
+    it('should allow adding complex thigns', function() {
+        map[a] = 'xyz';
+    });
 
-	it('should allow adding getting based on complex things 1',function(){
-		map[a] = 'xyz';
-		assert.equal(map[a],'xyz');
-	});
+    it('should allow adding getting based on complex things 1', function() {
+        map[a] = 'xyz';
+        assert.equal(map[a], 'xyz');
+    });
 
-	it('should allow adding getting based on complex things 1',function(){
-		map[a] = 'xyz';
-		assert.equal(map.get(a.toString()),'xyz');
-	});
+    it('should allow adding getting based on complex things 1', function() {
+        map[a] = 'xyz';
+        assert.equal(map.get(a.toString()), 'xyz');
+    });
 });
